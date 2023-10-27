1965a747386a98473762b3f0d10b188ce10e3358
ali <agirisen0@gmail.com>
2023-10-27 14:23:42 +0300

    harmonized to liderahenk

--- src/syncAD.py
+++ src/syncAD.py
@@ -24,8 +24,11 @@ ldap_admin_username = config.get('LDAP','ldap_admin_username')
 ldap_admin_password = config.get('LDAP','ldap_admin_password')
 ldap_base_dn = config.get('LDAP','ldap_base_dn')
 ldap_group_dn = config.get('LDAP','ldap_group_dn')
+ldap_user_group_dn = config.get('LDAP','ldap_user_group_dn')
 ldap_users_dn = config.get('LDAP','ldap_users_dn')
-
+ldap_agent_group_dn = config.get('LDAP','ldap_agent_group_dn')
+ldap_policy_group_dn = config.get('LDAP','ldap_policy_group_dn')
+ldap_role_group_dn = config.get('LDAP','ldap_role_group_dn')
 
 ldap_existing_groups = []
 spec_search = False
@@ -42,7 +45,7 @@ def search_lines(filename_ad, total_lines_ad, input_dn):
 
     server = Server(ldap_server, port=ldap_port)
     conn = Connection(server, user=ldap_admin_username, password=ldap_admin_password, auto_bind=True)
-    dn,cn,sn,givenName,email,phoneNumber,sAMAccountName,memberOf = " " , " ", " ", " ", " ", " ", " ", []
+    dn,cn,sn,givenName,email,phoneNumber,homePostalAddress,sAMAccountName,memberOf = " " , " ", " ", " ", " ", " ", " ", " ", []
     with open(filename_ad, 'r') as file:
         for line_number, line in enumerate(file, start=1):
             if line.startswith('#') or line.startswith(' '):
@@ -57,6 +60,7 @@ def search_lines(filename_ad, total_lines_ad, input_dn):
                             'sn': sn,
                             'telephoneNumber': phoneNumber,
                             'mail': mail,
+                            'homePostalAddress':homePostalAddress,
                             }
                             for attribute_name, expected_value in attributes_to_compare.items():
                                 comparison = conn.compare(dn, attribute_name, expected_value)
@@ -74,14 +78,14 @@ def search_lines(filename_ad, total_lines_ad, input_dn):
                                 print("No attribute modifications to perform for ",cn)
                             
                             for group in memberOf:
-                                group_dn = f"cn={group},{ldap_group_dn}"
+                                group_dn = f"cn={group},{ldap_user_group_dn}"
                                 result = conn.modify(group_dn, {'member': [(MODIFY_ADD, [dn])]})
                                 print(f"Successfully modified membership of the LDAP entry: {cn}")
                             search_filter_groups = f"(&(objectClass=groupOfNames)(member={dn}))"
-                            user_membership = conn.search(ldap_group_dn, search_filter=search_filter_groups)
+                            user_membership = conn.search(ldap_user_group_dn, search_filter=search_filter_groups)
                             if user_membership:
                                 for group in ldap_existing_groups:
-                                    group_dn = f"cn={group},{ldap_group_dn}"
+                                    group_dn = f"cn={group},{ldap_user_group_dn}"
                                     group_existing = conn.compare(group_dn, "member", dn)
                                     if group_existing:
                                         if group not in memberOf:
@@ -104,6 +108,7 @@ def search_lines(filename_ad, total_lines_ad, input_dn):
                             'sn': sn,
                             'telephoneNumber': phoneNumber,
                             'mail': mail,
+                            'homePostalAddress':homePostalAddress,
                             }
 
                             for attribute_name, expected_value in attributes_to_compare.items():
@@ -118,14 +123,14 @@ def search_lines(filename_ad, total_lines_ad, input_dn):
                                     print(f"Failed to modify the LDAP entry: {e}")
 
                             for group in memberOf:
-                                group_dn = f"cn={group},{ldap_group_dn}"
+                                group_dn = f"cn={group},{ldap_user_group_dn}"
                                 result = conn.modify(group_dn, {'member': [(MODIFY_ADD, [dn])]})
 
                             search_filter_group = f"(&(objectClass=groupOfNames)(member={dn}))"
-                            user_membership = conn.search(ldap_group_dn, search_filter=search_filter_group)
+                            user_membership = conn.search(ldap_user_group_dn, search_filter=search_filter_group)
                             if user_membership:
                                 for group in ldap_existing_groups:
-                                    group_dn = f"cn={group},{ldap_group_dn}"
+                                    group_dn = f"cn={group},{ldap_user_group_dn}"
                                     group_existing = conn.compare(group_dn, "member", dn)
                                     if group_existing:
                                         if group not in memberOf:
@@ -138,20 +143,19 @@ def search_lines(filename_ad, total_lines_ad, input_dn):
                         else:
                             filter_str = f'(uid={sAMAccountName})'
                             exist_uid = conn.search(search_base=ldap_users_dn, search_filter=filter_str, search_scope=SUBTREE, attributes=['cn'])
-                            if conn.search(f'{group_name},dc=alidap,dc=com', '(objectClass=*)'):
+                            if conn.search(f'{group_name},{ldap_base_dn}', '(objectClass=*)'):
                                 if exist_uid:
                                     entry = conn.entries[0]
                                     conn.delete(entry.entry_dn)
                                 object_class = ['inetOrgPerson','organizationalPerson','top','person']
                                 new_attributes = {
                                 'objectClass': object_class,
-                                'ou': 'Groups',
-                                'ou': 'Users',
                                 'cn': cn,
                                 'givenName': givenName,
                                 'sn': sn,
                                 'uid': sAMAccountName,
                                 'mail': mail,
+                                'homePostalAddress': homePostalAddress,
                                 'telephoneNumber': phoneNumber,
                                 }
                                 result = conn.add(dn, attributes=new_attributes)
@@ -159,7 +163,7 @@ def search_lines(filename_ad, total_lines_ad, input_dn):
                                     print(f"Failed to add user: {conn.result}")
                             else:
                                 if sync_group_mode:
-                                    groups_dn = ldap_group_dn 
+                                    groups_dn = ldap_base_dn 
                                     for directory in reversed(org_unit_list):
                                         group_str = f"{directory},{groups_dn}"
                                         groups_dn = group_str
@@ -172,19 +176,20 @@ def search_lines(filename_ad, total_lines_ad, input_dn):
                                 object_class = ['inetOrgPerson','organizationalPerson','top','person']
                                 new_attributes = {
                                 'objectClass': object_class,
-                                'ou': 'Groups',
-                                'ou': 'Users',
                                 'cn': cn,
                                 'givenName': givenName,
                                 'sn': sn,
                                 'uid': sAMAccountName,
                                 'mail': mail,
+                                'homePostalAddress': homePostalAddress,
                                 'telephoneNumber': phoneNumber,
                                 }
+
                                 result = conn.add(dn, attributes=new_attributes)
-                                #if not result:
-                                #    print(f"CHECK THE SYNC GROUP PREFERENCES MAY GROUP NOT EXISTING Failed to add user: {conn.result}")
-                    dn,cn,sn,givenName,email,phoneNumber,sAMAccountName,memberOf = " ", " ", " ", " ", " ", " ", " ", []
+                                for group in memberOf:
+                                    group_dn = f"cn={group},{ldap_user_group_dn}"
+                                    conn.modify(group_dn, {'member': [(MODIFY_ADD, [dn])]})
+                    dn,cn,sn,givenName,email,homePostalAddress,phoneNumber,sAMAccountName,memberOf = " ", " ", " ", " ", " ", " ", " ", " ", []
 
                 elif input_dn and total_lines_ad == line_number:
                     print(f"User is not found !!")
@@ -213,7 +218,6 @@ def search_lines(filename_ad, total_lines_ad, input_dn):
                     elif component.startswith('OU='):
                         formatted_components.append('ou=' + component[3:])
 
-                formatted_components.append('ou=Groups')
                 group_name = ','.join(org_unit_list)
                 new_dn = ','.join(formatted_components)
                 dn = (new_dn+","+ldap_base_dn)
@@ -236,16 +240,26 @@ def search_lines(filename_ad, total_lines_ad, input_dn):
             elif line.startswith('mail'):
                 mail = line.strip()
                 mail = content(mail)
+            elif line.startswith('homePostalAddress'):
+                homePostalAddress = line.strip()
+                homePostalAddress = content(homePostalAddress)
             elif line.startswith('telephoneNumber'):
                 phoneNumber = line.strip()
                 phoneNumber = content(phoneNumber)
             elif line.startswith('memberOf'):
                 group = line.strip()
                 components = group.split(",")
-                memberOf.append(components[0].split('=')[1])
+                group = components[0].split('=')[1]
+                if group == "Administrators":
+                    group = "adminGroups"
+                elif group == "Domain Admins":
+                    group = "domainAdminGroup"
+                else:
+                    group = ""
+                
+                if group:
+                    memberOf.append(group)
 
-            else:
-                pass
             progress = (line_number + 1) / total_lines_ad * 100
             print(f"Progress: {progress:.0f}%", end='', flush=True)
             backspaces = len(f"Progress ({progress:.0f}%)")
@@ -259,7 +273,7 @@ def getLdapGroups():
     conn = Connection(server, user=ldap_admin_username, password=ldap_admin_password, auto_bind=True)
 
     search_filter = "(objectClass=groupOfNames)"
-    result = conn.search(ldap_group_dn,search_filter,attributes=["cn"])
+    result = conn.search(ldap_user_group_dn,search_filter,attributes=["cn"])
     for entry in conn.entries:
         ldap_existing_groups.append(entry.cn)
 
@@ -272,7 +286,7 @@ def content(content):
 
 
 start_time = time.time()
-command_ad = f'ldapsearch -x -D {ad_username} -w {ad_password} -H ldap://{ad_server}:{ad_port} -b {ad_base_dn} "(&(objectClass=person)(objectCategory=person)(!(sAMAccountName=krbtgt))(!(sAMAccountName=Administrator))(!(sAMAccountName=Guest)) )" -s sub -E pr=1000/noprompt  cn sn telephoneNumber mail givenName sAMAccountName memberOf > ad_users.ldif'
+command_ad = f'ldapsearch -x -D {ad_username} -w {ad_password} -H ldap://{ad_server}:{ad_port} -b {ad_base_dn} "(&(objectClass=person)(objectCategory=person)(!(sAMAccountName=krbtgt))(!(sAMAccountName=Administrator))(!(sAMAccountName=Guest)) )" -s sub -E pr=1000/noprompt  cn sn telephoneNumber mail homePostalAddress givenName sAMAccountName memberOf > ad_users.ldif'
 print("Fetching - Active Directory Users...")
 result_ad = subprocess.run(command_ad, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
 
@@ -282,7 +296,7 @@ if result_ad.returncode == 0:
     total_lines_ad = int(subprocess.check_output(["wc", "-l", "ad_users.ldif"]).split()[0])
 else:
     print("Failed with the following error:")
-    print(result.stderr)
+    print(result_ad.stderr)
 
 
 file_path = 'ad_users.ldif'
